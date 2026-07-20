3e08cbf81c7fefe52bb9f63885594c6897b43466
Anubhav Shrivastava <anubhav.shri14@gmail.com>
2026-07-20 11:06:48 +0530

    Test Complete

--- tests/FormFill.spec.js
+++ tests/FormFill.spec.js
@@ -1,5 +1,7 @@
 import { test, expect } from '@playwright/test'
 test("Dummy Form Fill", async ({ page }) => {
-    await page.goto("https://selectorshub.com/xpath-practice-page/");
-    await page.getByPlaceholder("Enter email").fill("anubhav.shri14@gmail.com")
+    test.slow()
+    await page.goto("https://selectorshub.com/xpath-practice-page/"/*,{waitUntil:'domcontentloaded'}*/);
+    await page.locator("//input[@placeholder='Enter email']").fill("anubhav.shri14@gmail.com");
+    await page.locator("//input[@name='Password']").fill("abcdefgh")
 })
